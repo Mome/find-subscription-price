68b4928e3b017958d5e47d2d2b4e4512a02e2f9a
Moritz Meier <moritzmeier@aol.com>
2017-07-25 18:37:50 +0200

    introduced questions

---
+++ chatbot_shell.py
@@ -0,0 +1,91 @@
+import cmd
+
+import pandas as pd
+import rasa_nlu.model as rasa_model
+import rasa_nlu.config as rasa_config
+
+from chatbot import PreferenceModel, Chatbot
+from utils import find_latest_model
+
+DEBUG = True
+
+
+class ChatbotShell(cmd.Cmd):
+    prompt = '» '
+    escape_chrs =  (':','!')
+    intro = '\n--- enter %shelp to get help ---\n\nbot: How can I help you?' % escape_chrs[0]
+    
+    def __init__(self, chatbot):
+        super().__init__()
+        self.chatbot = chatbot
+
+    def precmd(self, line):
+        if line.startswith(self.escape_chrs):
+            return line[1:]
+        else:
+            return 'say ' + line
+    
+    def do_say(self, line):
+        intent, response = self.chatbot.process_message(line)
+        
+        if intent is 'info':
+            print('bot:', response, f'({intent})', end='\n'*2)
+            intent = None
+       
+        if intent is None:
+            intent, response = self.chatbot.generate_question(line)
+        
+        if not intent:
+            intent, response = self.chatbot.intent_recommendation(line)
+        
+        print('bot:', response, f'({intent})', end='\n'*2)
+        
+        if intent == "goodbye":
+            return True
+        
+    def do_exit(self, line):
+        return True
+    
+    def do_get(self, line):
+        """Prints first var with given name from shell, chatbot, model or globals."""
+        
+        line = line.replace(' ', '_')
+        for vars_ in [self, self.chatbot, self.chatbot.pref_model, globals()]:
+            if not isinstance(vars_, dict):
+                vars_ = vars(vars_)
+            if line in vars_:
+                print('\n', vars_[line], '\n', sep='')
+                break
+        else:
+            print('cannot find identifier:', line)
+    
+    def do_debug(self, line):
+        self.chatbot.debug = not self.chatbot.debug
+        print('debug mode:', 'on' if self.chatbot.debug else 'off')
+        
+    def default(self, line):
+        self.do_get(line)
+
+        
+# ---------------------------------------------------------------------------- #
+
+
+def main():
+    data = pd.read_table('data.csv', index_col=0)
+    
+    pref_model = PreferenceModel(data)
+    
+    
+    model_path = find_latest_model()
+    print('Loading rasa model:', model_path, '...', end=' ', flush=True)
+    rasa_interpreter = rasa_model.Interpreter.load(
+            model_metadata = rasa_model.Metadata.load(model_path),
+        config = rasa_config.RasaNLUConfig("config_spacy.json"))
+    print('done')
+    
+    chatbot = Chatbot(pref_model, rasa_interpreter, debug=DEBUG)
+
+    ChatbotShell(chatbot).cmdloop()
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,37 @@
+import os
+
+def find_latest_model(models_dir='./models'):
+    newes = None
+    time = 0.0
+    for name in os.listdir(models_dir):
+        path = os.path.join(models_dir, name)
+        this_time = os.path.getctime(path)
+        if this_time > time:
+            time = this_time
+            newest = path
+    return path
+
+
+def render_enum(seq, conj='and'):
+    seq = tuple(seq)
+    if len(seq) == 0:
+        return ''
+    if len(seq) == 1:
+        return seq[0]
+    head = ', '.join(seq[:-1])
+    tail = seq[-1]
+    return f'{head} {conj} {tail}'
+
+
+def has_negation(sent):
+    # maybe add sentiment analysis here ...
+    return 'not' in sent
+
+
+def normalize_text(text):
+    text = text.lower()
+    
+    # replace synonyms
+    words = (synonyms.get(w, w) for w in nlp.tokenize(text))
+    
+    return ' '.join(text)
